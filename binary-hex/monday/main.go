fa964d5a5e3198cdaf19d881e329f856891a5073
Liberty Ebikade <ebikadeliberty7@gmail.com>
2023-07-06 16:02:17 +0100

    first commit

---
+++ binary-hex/monday/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var Today = "MONDAY"
+	fmt.Println(Today)
+
+	var today = "Monday"
+
+	na := "monday"
+
+	fmt.Println(na)
+
+	fmt.Println(string([]byte(today)))
+}
---
+++ binary-hex/thursday/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("THURSDAY")
+
+	today := "THURSDAY"
+	fmt.Println(string([]byte(today)))
+}

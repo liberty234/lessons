b23a863a183887184d3dfbba6038fd2f74e94c26
Liberty Ebikade <ebikadeliberty7@gmail.com>
2023-07-23 12:59:26 +0100

    added binary hex

---
+++ binary-hex/monday/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var Today = "MONDAY"
+	fmt.Println(Today)
+
+	var today = "Monday"
+
+	na := "monday"
+	for i := 0; i < 5; i++ {
+
+		fmt.Println(na)
+
+	}
+
+	fmt.Println(string([]byte(today)))
+}
---
+++ binary-hex/saturday/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("SATURDAY")
+	today := "SATURDAY"
+	fmt.Println([]byte(today))
+
+}

d7d059c81a9012cfad43059351bdf06ad52b7931
Liberty Ebikade <ebikadeliberty7@gmail.com>
2023-07-24 10:21:43 +0100

    added a public and private function with a new data export package

--- tour-of-go/exports/data/data_structure.go
+++ tour-of-go/exports/data/data_structure.go
@@ -3,6 +3,6 @@ package data
 import "fmt"
 
 func Class() {
-	classes := class(45, 23)
-	fmt.Println("The number of class1 and class2 = ", classes)
+	class12 := class(45, 23)
+	fmt.Println("The number of class1 and class2 = ", class12)
 }

9d4a5f1d9959bdcff6154205935b8235b936a87c
Liberty Ebikade <ebikadeliberty7@gmail.com>
2023-07-28 22:39:22 +0100

    used files as exported package with private and public

---
+++ tour-of-go/exports/files/files.go
@@ -0,0 +1,17 @@
+package files
+
+import "fmt"
+
+var Files = "This is my files"
+
+func NameOfFiles() {
+	do := "police files"
+	fmt.Println("file:", do)
+
+}
+
+func ValuesCheck() {
+	fmt.Println("percent:", check)
+	valueCheck()
+
+}
---
+++ tour-of-go/exports/files/files_check.go
@@ -0,0 +1,12 @@
+package files
+
+import "fmt"
+
+var check = 90 % 12
+
+func valueCheck() {
+	x := 190
+	y := 65
+	fmt.Println("valueCheck:", x*y)
+
+}

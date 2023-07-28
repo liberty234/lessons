a9e8ddfd41358800fc56cc12a6e943b33b7172f9
Liberty Ebikade <ebikadeliberty7@gmail.com>
2023-07-28 22:48:30 +0100

    added a state export package and used variable and function in public and private

---
+++ tour-of-go/exports/state/state.go
@@ -0,0 +1,19 @@
+package state
+
+import "fmt"
+
+var StateOfOrigin = "EDO STATE"
+
+func StateName() {
+	e := "EDO STATE"
+	a := "ABIA STATE"
+	l := "LAGOS STATE"
+	an := "ANAMBRA STATE"
+	fmt.Println("Some state in Nigeria:", e, a, l, an)
+
+}
+func Origin() {
+	fmt.Println("Place:", state)
+	origin()
+
+}
---
+++ tour-of-go/exports/state/state_origin.go
@@ -0,0 +1,12 @@
+package state
+
+import "fmt"
+
+var state = "live in benin edo state"
+
+func origin() {
+	e := "Edo"
+	s := "State"
+	fmt.Println("Origin:", e+s)
+
+}

20a6129348e48b4173ba7dad5b9329955ea2e5b3
Liberty Ebikade <ebikadeliberty7@gmail.com>
2023-07-29 07:59:51 +0100

    created an operation  export package and  used variable as  public and private funtion

---
+++ tour-of-go/exports/operation/operation.go
@@ -0,0 +1,18 @@
+package operation
+
+import "fmt"
+
+var Opramin = "we have a great news today"
+
+func Opera() {
+	op := [...]string{"News Talk", "program growth"}
+	fmt.Println("Parsing package:", op)
+
+}
+
+func Divide() {
+
+	fmt.Println("Ans:", solve)
+	div := divide(80, 20)
+	fmt.Println("Divition;", div)
+}
---
+++ tour-of-go/exports/operation/operation_help.go
@@ -0,0 +1,7 @@
+package operation
+
+var solve = 15 * 2
+
+func divide(z int, k int) int {
+	return z / k
+}

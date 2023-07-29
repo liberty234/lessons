2b1e680d7e3b5b0eb9ad4317c4f82c7f97a4d34c
Liberty Ebikade <ebikadeliberty7@gmail.com>
2023-07-29 20:55:55 +0100

    created a webs package which I used as public and private function

---
+++ tour-of-go/exports/webs/webs.go
@@ -0,0 +1,18 @@
+package webs
+
+import "fmt"
+
+var NewName = "Mr Ojo is the new client"
+
+func PotName() {
+	do := "joseph ogodo"
+	fmt.Println("Client:", do)
+
+}
+
+func Values() {
+	fmt.Println("percent:", check)
+	c := call(34, 12)
+	fmt.Println("plus:", c)
+
+}
---
+++ tour-of-go/exports/webs/webs_parser.go
@@ -0,0 +1,7 @@
+package webs
+
+var check string = "big Man"
+
+func call(x, y int) int {
+	return x + y
+}

e04041b26095a3a1542c6ef5b7392bb4979fdf4a
Liberty Ebikade <ebikadeliberty7@gmail.com>
2023-07-29 20:58:31 +0100

    created a saver package which I used as public and private function

---
+++ tour-of-go/exports/saver/saver.go
@@ -0,0 +1,18 @@
+package saver
+
+import "fmt"
+
+var Saved = "web saver"
+
+func WebName() {
+
+	w := "www.libohop.com"
+	fmt.Println("web:", w)
+
+}
+
+func Website() {
+	fmt.Println("website", website)
+	war()
+
+}
---
+++ tour-of-go/exports/saver/saver_mod.go
@@ -0,0 +1,11 @@
+package saver
+
+import "fmt"
+
+var website = "www.greatness.com"
+
+func war() {
+	w := "self gain"
+	fmt.Println("Title:", w)
+
+}

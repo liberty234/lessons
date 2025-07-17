a4c01c280bde86b744e2d392aa688b03d3133690
Liberty Ebikade <ebikadeliberty7@gmail.com>
2025-07-17 23:11:16 +0100

    explained package, functions, varianles with a comment

--- basic_structs/modern-go/main.go
+++ basic_structs/modern-go/main.go
@@ -1,19 +1,22 @@
+// package
 package main
 
+//import
 import "fmt"
 
+// functions; all code must inside the main functions
 func main() {
 	var b = 78
 
 	a := 73
-
+	// local variables
 	var c = a + b
 
 	fmt.Println("result:", c)
 	fmt.Println(c, q, i, b, e, r, d)
 }
 
-// variable
+// Global variable
 
 var q, i, e, r int = 4, 7, 9, 2
 var d bool = true
